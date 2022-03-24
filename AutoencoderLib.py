351b8a493b634948e51926bebec8eeba3c5f1302
Francisco Javier Vicente Martínez <fcojavier.vicente1@alu.uclm.es>
2022-03-24 20:42:12 +0100

    Añadida funcion de compilacion y entrenamiento. Relacionado con #16

--- AutoencoderLib.py
+++ AutoencoderLib.py
@@ -286,7 +286,7 @@ def build_branched_class_discriminator(dim_latente:int, clases:int, depth = 2, w
 # COMPILADO DE MODELOS
 # -----------------------------------------------------------
 
-def assemble_AAE(dim_latente:int, img_shape:tuple, enc_model:Model=build_dense_encoder, dec_model:Model=build_dense_decoder, disc_model:Model = build_discriminator, 
+def assemble_AAE_twoPhased(dim_latente:int, img_shape:tuple, enc_model:Model=build_dense_encoder, dec_model:Model=build_dense_decoder, disc_model:Model = build_discriminator, 
                 compilation_kwargs:dict={}, enc_kwargs:dict={}, dec_kwargs:dict={}, disc_kwargs:dict={}) -> tuple:
     """
     Metodo para la construccion. Devuelve una tupla con los modelos del encoder, decoder, discriminador y autoencoder adversario en ese orden.\n
@@ -345,6 +345,61 @@ def assemble_AAE(dim_latente:int, img_shape:tuple, enc_model:Model=build_dense_e
     a_autoencoder = keras.Model(aae_input, [reconstructed, validez])
     a_autoencoder.compile(loss=[cp["ae_loss"], cp["disc_loss"]], optimizer=cp["optimizer"])#, loss_weights=[0.999, 0.001])
     return (encoder, decoder, discriminator, a_autoencoder)
+
+def assemble_AAE_threePhased(
+    dim_latente:int, img_shape:tuple, enc_model:Model=build_dense_encoder, dec_model:Model=build_dense_decoder, disc_model:Model = build_discriminator, compilation_kwargs:dict={}, enc_kwargs:dict={}, dec_kwargs:dict={}, disc_kwargs:dict={}
+    ) -> tuple:
+    """
+    Metodo para la construccion. Devuelve una tupla con los modelos del encoder, decoder, discriminador, autoencoder y encoder-discriminador adversario en ese orden.\n
+    dim_latente: dimensiones del espacio latente \n
+    img_shape: tamaño de la imagen \n
+    enc_model: funcion de construccion del encoder \n
+    dec_model: funcion de construccion del decoder\n
+    disc_model: funcion de construccion del discriminador \n
+    compilation_kwargs: agrupacion de argumentos "ae_loss", "disc_loss", "optimizer" para la complilacion del discriminador y el autoencoder \n
+    enc_kwargs: argumentos admitidos por el encoder \n
+    dec_kwargs: argumentos admitidos por el decoder\n
+    disc_kwargs: argumentos admitidos por el discriminador
+    """
+    # Discriminador
+    discriminator = disc_model(dim_latente=dim_latente, **disc_kwargs)
+    cp = {"ae_loss":losses.mean_squared_error, 
+          "disc_loss": losses.binary_crossentropy, 
+          "optimizer" : keras.optimizers.Adam(0.0002, 0.5)
+         } #cp = compilation params
+    cp.update(compilation_kwargs)
+    discriminator.compile(loss=cp["disc_loss"], metrics="accuracy")
+
+    # Encoder y decoder
+    encoder = enc_model(dim_latente=dim_latente, img_shape=img_shape, **enc_kwargs)
+    decoder = dec_model(dim_latente=dim_latente, img_shape=img_shape,**dec_kwargs)
+
+    # Autoencoder
+    # el encoder toma un imagen y la codifica y el decoder toma la codificacion e intenta regenerar la imagen
+    img = Input(shape=img_shape, name="data")
+    encoded = encoder(img)
+    autoencoder = decoder(encoded)
+    autoencoder.compile(loss=cp["ae_loss"], optimizer=cp["optimizer"])
+
+    # para la distribucion del encoder solo queremos entrenar el generador, no el discriminador
+    discriminator.trainable=False
+
+    if "clases" in disc_kwargs.keys():
+        clase = Input(shape=disc_kwargs["clases"], name = "labels")
+        disc_input = [encoded, clase]
+        enc_input = [img, clase]
+    else:
+        disc_input = encoded
+        enc_input = img
+
+    # El discriminador evalua la validez de la codificacion
+    validez = discriminator(disc_input)
+
+    # Encoder-discriminador adversario 
+    encosriminator = keras.Model(enc_input, validez)
+    encosriminator.compile(optimizer=cp["optimizer"], loss='binary_crossentropy', metrics=['accuracy'])
+
+    return (encoder, decoder, discriminator, autoencoder, encosriminator)
 
 # -----------------------------------------------------------
 # GENERACION DE EJEMPLOS
@@ -682,3 +737,72 @@ def fit_AAE_twoPhased(dim_latente:int, aae:tuple, dataset:dict, epochs=12, batch
         generate_samples(dim_latente, decoder, epoch, ruta=ruta, nombre=nombre, show=((epoch+1)==epochs))
     return history.transpose(1,0)
 
+def fit_AAE_threePhased(dim_latente:int, aae:tuple, dataset:dict, epochs=12, batch_size=100, sample_interval=100, ruta="Resultados/pruebasAAE", nombre="pAAE", verbose=True,
+            truth=true_sampler, truth_kwargs={}, falsehood=fake_sampler):
+    """
+    Entrenamiento en dos fases, devuelve historial de entrenamiento. 1-Discriminador 2-Autoencoder. 
+    En la fase dos se entrenan al mismo tiempo la regeneracion de las imagenes y el ajuste del espacio latente a la distribucion en la que se haya entrenado el discriminador.\n
+    dim_latente: dimensiones del espacio latente \n
+    aae: tupla con los modelos\n
+    dataset: conjunto de datos de entrenamiento\n
+    epochs: numero de epochs\n
+    batch_size: tamaño del lote de imagenes con las que entrenar al mismo tiempo\n
+    sample_interval: intervalo de salida por consola de datos\n
+    ruta: raiz para el guardado de resultados\n
+    nombre: nombre descriptivo\n
+    verbose: si imprime salida por consola o no\n
+    truth: funcion de la distribucion que debe seguir el discriminador\n
+    truth_kwargs: argumentos para la funcion de la distribucion\n
+    falsehood: funcion de toma de muestras con el encoder
+    """
+    elements = len(dataset["data"])
+    totalSteps = ceil(elements/batch_size)
+
+    dataset = tf.data.Dataset.from_tensor_slices(dataset)
+    dataset = dataset.shuffle(elements, seed=2022)
+    dataset = dataset.batch(batch_size)
+
+    history = np.empty([0,4])
+
+    encoder, decoder, discriminator, autoencoder, encoscriminador=aae
+    
+    valid = np.ones((batch_size, 1))
+    fake = np.zeros((batch_size, 1))
+
+    print("Entrenando: "+ruta)
+    
+    for epoch in range(epochs):
+        start = timer()
+        for step, imgs in enumerate(dataset):
+            #Espacios latentes "reales" y "falsos" para el discriminador
+            latent_fake = falsehood(imgs=imgs, encoder=encoder, **truth_kwargs)
+            latent_true = truth(dim_latente=dim_latente, batch_size=batch_size, **truth_kwargs)
+
+            #entrenamos el discriminador
+            dis_loss_real = discriminator.train_on_batch(latent_true, valid)
+            dis_loss_fake = discriminator.train_on_batch(latent_fake, fake)
+            dis_avg_loss = 0.5*np.add(dis_loss_fake, dis_loss_real)
+
+            # entrenamos al regenerador
+            if "nclases" in truth_kwargs:
+                imgs["labels"]=onehotify(imgs["labels"], truth_kwargs["nclases"])
+            ae_loss = autoencoder.train_on_batch(imgs, imgs) 
+            
+            # Entrenamos el generador, la idea es que el encoder genere "imagenes validas"
+            ed_loss=encoscriminador.train_on_batch(imgs, valid)
+
+            # Guardamos el progreso
+            history = np.append(history, [np.append(dis_avg_loss[:2], ae_loss, ed_loss[1])], axis=0)
+            
+            # monitorizamos el progreso
+            if step*((step+1) % sample_interval)==0 and verbose:
+                progressPercent=step/totalSteps
+                bar=ceil(progressPercent*10)
+                elapsed = timer() - start
+                print("E%d <" % (epoch)+chr(9608)*bar+" "*(10-bar)+"> %d%% DISC: [loss: %f, acc: %.2f%%] AE: [loss: %f] EN-DI [acc: %.2f%%] %.2fs\t\t" % (ceil(100*progressPercent), dis_avg_loss[0], 100*dis_avg_loss[1], ae_loss, ed_loss[1], elapsed), end="\r")
+        if verbose:
+            print("")
+        # Hacemos una muestra visual
+        generate_samples(dim_latente, decoder, epoch, ruta=ruta, nombre=nombre, show=((epoch+1)==epochs))
+    
+    return history.transpose(1,0)
