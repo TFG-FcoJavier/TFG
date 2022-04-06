947de9452a7ddb62a8312caae49422a780163e50
Francisco Javier Vicente Martínez <fcojavier.vicente1@alu.uclm.es>
2022-04-06 19:27:18 +0200

    Simplificado assemble_AAE_twophased Añadidos argumentos, quitada logica innecesaria

--- lib/compilado.py
+++ lib/compilado.py
@@ -9,7 +9,7 @@ from keras import losses
 from densa import build_dense_decoder, build_dense_encoder
 from discriminadores import build_discriminator
 
-def assemble_AAE_twoPhased(dim_latente:int, img_shape:tuple, enc_model:Model=build_dense_encoder, enc_kwargs:dict={}, dec_model:Model=build_dense_decoder, dec_kwargs:dict={}, disc_model:Model = build_discriminator, disc_kwargs:dict={}, compilation_kwargs:dict={}, loss_weights=[1, 1]) -> tuple:
+def assemble_AAE_twoPhased(dim_latente:int, img_shape:tuple, enc_model:Model=build_dense_encoder, enc_kwargs:dict={}, dec_model:Model=build_dense_decoder, dec_kwargs:dict={}, disc_model:Model = build_discriminator, disc_kwargs:dict={}, ae_loss=losses.mean_squared_error, disc_loss= losses.binary_crossentropy, optimizer = keras.optimizers.Adam(0.0002, 0.5), loss_weights=[1, 1]) -> tuple:
     """
     Metodo para la construccion. Devuelve una tupla con los modelos del encoder, decoder, discriminador y autoencoder adversario en ese orden.\n
     dim_latente: dimensiones del espacio latente \n
@@ -17,31 +17,17 @@ def assemble_AAE_twoPhased(dim_latente:int, img_shape:tuple, enc_model:Model=bui
     enc_model: funcion de construccion del encoder \n
     dec_model: funcion de construccion del decoder\n
     disc_model: funcion de construccion del discriminador \n
-    compilation_kwargs: agrupacion de argumentos "ae_loss", "disc_loss", "optimizer" para la complilacion del discriminador y el autoencoder \n
     enc_kwargs: argumentos admitidos por el encoder \n
     dec_kwargs: argumentos admitidos por el decoder\n
     disc_kwargs: argumentos admitidos por el discriminador
     """
-    #Parameters
-    disc_params = {"dim_latente":dim_latente}
-    disc_params.update(disc_kwargs)
-    enc_params = {"dim_latente":dim_latente, "img_shape":img_shape}
-    enc_params.update(enc_kwargs)
-    dec_params = {"dim_latente":dim_latente, "img_shape":img_shape}
-    dec_params.update(dec_kwargs)
-
-    cp = {"ae_loss":losses.mean_squared_error, 
-          "disc_loss": losses.binary_crossentropy, 
-          "optimizer" : keras.optimizers.Adam(0.0002, 0.5)
-         } #cp = compilation params
-    cp.update(compilation_kwargs)
     # Discriminador
-    discriminator = disc_model(**disc_params)
-    discriminator.compile(loss=cp["disc_loss"], metrics="accuracy")
+    discriminator = disc_model(dim_latente=dim_latente, **disc_kwargs)
+    discriminator.compile(loss=disc_loss, metrics="accuracy")
 
     # Encoder y decoder
-    encoder = enc_model(**enc_params)
-    decoder = dec_model(**dec_params)
+    encoder = enc_model(dim_latente=dim_latente,img_shape=img_shape, **enc_kwargs)
+    decoder = dec_model(dim_latente=dim_latente,img_shape=img_shape, **dec_kwargs)
 
     # Autoencoder
     # el encoder toma un imagen y la codifica y el decoder toma la codificacion e intenta regenerar la imagen
@@ -49,8 +35,8 @@ def assemble_AAE_twoPhased(dim_latente:int, img_shape:tuple, enc_model:Model=bui
     encoded = encoder(img)
     reconstructed = decoder(encoded)
     
-    if "clases" in disc_params.keys():
-        clase = Input(shape=disc_params["clases"], name = "labels")
+    if "clases" in disc_kwargs.keys():
+        clase = Input(shape=disc_kwargs["clases"], name = "labels")
         disc_input = [encoded, clase]
         aae_input = [img, clase]
     else:
@@ -65,7 +51,7 @@ def assemble_AAE_twoPhased(dim_latente:int, img_shape:tuple, enc_model:Model=bui
 
     # Autoencoder adversario 
     a_autoencoder = keras.Model(aae_input, [reconstructed, validez])
-    a_autoencoder.compile(loss=[cp["ae_loss"], cp["disc_loss"]], optimizer=cp["optimizer"], loss_weights=loss_weights)
+    a_autoencoder.compile(loss=[ae_loss, disc_loss], optimizer=optimizer, loss_weights=loss_weights)
     return (encoder, decoder, discriminator, a_autoencoder)
 
 def assemble_AAE_threePhased(
